989d57471bc31d510b5ea5cd1d918aaaa0206b3d
piperuao26 <piperuao26@gmail.com>
2024-05-13 19:54:46 -0500

    Bucket

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__, template_folder='taller2/templates')
 app.register_blueprint(pokeneas_bp)
 
 pokeneas = [
-    {"id": 1, "nombre": "Pikachu", "altura": 0.4, "habilidad": "Electricidad", "imagen": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/025.png", "frase_filosofica": "A pesar de todo sigo de pie"},
-    {"id": 2, "nombre": "Bulbasaur", "altura": 0.7, "habilidad": "Hierba", "imagen": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png", "frase_filosofica": "whigga"},
-    {"id": 3, "nombre": "Charmander", "altura": 0.6, "habilidad": "Fuego", "imagen": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/004.png", "frase_filosofica": "ya no mas morbojoooooooooon"},
-    {"id": 4, "nombre": "Squirtle", "altura": 0.5, "habilidad": "Agua", "imagen": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/007.png", "frase_filosofica": "how can i be gae, mi bitch is homophobic"},
-    {"id": 5, "nombre": "Jigglypuff", "altura": 0.5, "habilidad": "Canto", "imagen": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/039.png", "frase_filosofica": "peruan depresion"},
-    {"id": 6, "nombre": "Eevee", "altura": 0.3, "habilidad": "Evolución", "imagen": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/133.png", "frase_filosofica": "yo kreo que los fans de messi y cristiano deberiamos dejar de compararlos y besarnos entre todos"},
-    {"id": 7, "nombre": "Snorlax", "altura": 2.1, "habilidad": "Dormir", "imagen": "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/143.png", "frase_filosofica": "small dick, big feelings"}
+    {"id": 1, "nombre": "Pikachu", "altura": 0.4, "habilidad": "Electricidad", "imagen": "https://storage.googleapis.com/pokenea-1/pikashu.png", "frase_filosofica": "A pesar de todo sigo de pie"},
+    {"id": 2, "nombre": "Bulbasaur", "altura": 0.7, "habilidad": "Hierba", "imagen": "https://storage.googleapis.com/pokenea-1/bulbasaurio.png", "frase_filosofica": "whigga"},
+    {"id": 3, "nombre": "Charmander", "altura": 0.6, "habilidad": "Fuego", "imagen": "https://storage.googleapis.com/pokenea-1/cristinini.png", "frase_filosofica": "ya no mas morbojoooooooooon"},
+    {"id": 4, "nombre": "Squirtle", "altura": 0.5, "habilidad": "Agua", "imagen": "https://storage.googleapis.com/pokenea-1/cojone.png", "frase_filosofica": "how can i be gae, mi bitch is homophobic"},
+    {"id": 5, "nombre": "Jigglypuff", "altura": 0.5, "habilidad": "Canto", "imagen": "https://storage.googleapis.com/pokenea-1/jiggly.png", "frase_filosofica": "peruan depresion"},
+    {"id": 6, "nombre": "Eevee", "altura": 0.3, "habilidad": "Evolución", "imagen": "https://storage.googleapis.com/pokenea-1/perro.png", "frase_filosofica": "yo kreo que los fans de messi y cristiano deberiamos dejar de compararlos y besarnos entre todos"},
+    {"id": 7, "nombre": "Snorlax", "altura": 2.1, "habilidad": "Dormir", "imagen": "https://storage.googleapis.com/pokenea-1/drome.png", "frase_filosofica": "small dick, big feelings"}
 ]
 
 if __name__ == '__main__':
