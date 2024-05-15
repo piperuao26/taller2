e91150b937f97a6730610f4c6dcf6c44cb682b9c
piperuao26 <piperuao26@gmail.com>
2024-05-15 09:49:22 -0500

    app

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from routes import pokeneas_bp
+from flask import Flask, jsonify, render_template
+from random import choice
 import os
 
-app = Flask(__name__, template_folder='taller2/templates')
-app.register_blueprint(pokeneas_bp)
+app = Flask(__name__)
+
 
 pokeneas = [
     {"id": 1, "nombre": "Pikachu", "altura": 0.4, "habilidad": "Electricidad", "imagen": "https://storage.googleapis.com/pokenea-1/pikashu.png", "frase_filosofica": "A pesar de todo sigo de pie"},
@@ -14,6 +14,19 @@ pokeneas = [
     {"id": 6, "nombre": "Eevee", "altura": 0.3, "habilidad": "Evolución", "imagen": "https://storage.googleapis.com/pokenea-1/perro.png", "frase_filosofica": "yo kreo que los fans de messi y cristiano deberiamos dejar de compararlos y besarnos entre todos"},
     {"id": 7, "nombre": "Snorlax", "altura": 2.1, "habilidad": "Dormir", "imagen": "https://storage.googleapis.com/pokenea-1/drome.png", "frase_filosofica": "small dick, big feelings"}
 ]
+
+@app.route('/')
+def json_pokenea():
+    container_id = os.uname()[1]
+    pokenea = choice(pokeneas)
+    data = {
+        "id": pokenea["id"],
+        "nombre": pokenea["nombre"],
+        "altura": pokenea["altura"],
+        "habilidad": pokenea["habilidad"],
+        "id_contenedor": container_id  # Aquí puedes obtener el id del contenedor de alguna forma
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
